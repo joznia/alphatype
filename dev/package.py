9c449ae4b7b1bc6c5c4bb9f62afbf3f768a3380e
joznia <mail@joznia.com>
2021-02-06 14:32:59 -0800

    Update packager

--- dev/package.py
+++ dev/package.py
@@ -1,10 +1,16 @@
 import os
 
+distdirex = os.path.isdir("./dist")
+if distdirex == False:
+    os.mkdir("./dist")
+
 print("compiling alphatype to bytecode...")
 os.system("python -m py_compile alphatype.py")
 
-os.system("git tag -l")
-tagver = input("choose a tag to package: ")
+tagver = input("create a new tag (e.g. v1.2.9): ")
+tagmsg = input("tag message: ")
+os.system('''git tag -a %s -m "%s" ''' % (tagver, tagmsg))
+
 print("renaming compiled file...")
 os.chdir("__pycache__")
 try:
